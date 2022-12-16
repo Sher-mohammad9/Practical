7b3ed7a9e027d98cc6f6383ea2cace37fe741e25
sher-mohammad9 <hassan.deshwali0@gmail.com>
2022-12-16 18:59:44 +0530

    OOPs programms

---
+++ OOPs-programm/Constructor.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// Constructor 
+
+const map = function(parking,flor,furniture,flat){
+	this.parking = parking;
+	this.flor = flor;
+	this.furniture = furniture;
+	this.flat = flat;
+	console.log(this)
+}
+const house1 = new map("car-bike",2,"Chair-Table","3BHK")
+console.log(house1)
+
+const house2 = new map("Space",3,"Sofa","2BHK")
+console.log(house2)
+
+const person = function(house,school,market,game){
+    this.house = house;
+    this.school = school;
+    this.market = market;
+    this.game = game;
+}
+
+const person1 = new person("Son","Student","Customer","Player");
+console.log(person1)
+
+const person2 = new person("Father","Teacher","Customer","Player");
+console.log(person2)
---
+++ OOPs-programm/ES6_Class.js
@@ -0,0 +1,16 @@
+"use strict"
+
+class Animal{
+    constructor (name,eating,work){
+          this.name = name;
+          this.eating = eating;
+          this.work = work;
+          console.log(this)
+    }
+} 
+
+const lion = new Animal("Lion","Meat","Hunting");
+// console.log(lion);
+
+const tiger = new Animal("Tiger","Meat","Hunting")
+// console.log(tiger)
