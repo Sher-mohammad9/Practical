d1f8a70e38210b90b50dc74526b7dbaccbdd64b7
sher-mohammad9 <hassan.deshwali0@gmail.com>
2022-12-16 19:04:48 +0530

    OOPs programms

--- OOPs-programm/ES6_Class.js
+++ OOPs-programm/ES6_Class.js
@@ -5,12 +5,12 @@ class Animal{
           this.name = name;
           this.eating = eating;
           this.work = work;
-          console.log(this)
+          // console.log(this)
     }
 } 
 
 const lion = new Animal("Lion","Meat","Hunting");
-// console.log(lion);
+console.log(lion);
 
 const tiger = new Animal("Tiger","Meat","Hunting")
-// console.log(tiger)
+console.log(tiger)
