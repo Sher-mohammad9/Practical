896413f05e9624b1c79f7fe42d8122e4b19f4f8c
sher-mohammad9 <hassan.deshwali0@gmail.com>
2022-12-20 16:28:59 +0530

    Create Inheritance

--- OOPs-programm/Constructor.js
+++ OOPs-programm/Constructor.js
@@ -28,3 +28,26 @@ console.log(person1)
 
 const person2 = new person("Father","Teacher","Customer","Player");
 console.log(person2)
+
+// Inheritance with Constructor
+
+const Animal2 = function (name,color){
+	   this.name = name;
+	   this.color = color;
+}
+Animal2.prototype.display = function(){
+    console.log(`${this.color} color ${this.name} and are ${this.legs} legs`)	
+}
+
+const Cow = function(name1,color1,legs){
+	this.legs = legs;
+	Animal2.call(this,name1,color1)
+}
+
+Cow.prototype = Animal2.prototype;
+
+const ania = new Animal2("cow","white")	
+console.log(ania)
+const coww = new Cow("cow1","white1",4)
+coww.display();
+console.log(coww)
